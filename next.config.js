ab0525f72abd052017ebef64fc4defcbbe090a8c
gyorutan <waou1998@gmail.com>
2023-05-15 09:50:58 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   experimental: {
     appDir: true,
     swcPlugins: [["next-superjson-plugin", {}]],
   },
   images: {
     domains: [
-      'res.cloudinary.com', 
-      'avatars.githubusercontent.com',
-      'lh3.googleusercontent.com'
-    ]
-  }
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
 };
 
 module.exports = nextConfig;
